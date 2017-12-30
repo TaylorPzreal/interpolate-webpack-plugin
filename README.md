ae95ecf147b61fb27e7f11e1b7d41e96ff19add6
TaylorPzreal <taylorpzreal@gmail.com>
2017-12-30 11:29:09 +0800

    feat: InterpolateWebpackPlugin

---
+++ README.md
@@ -0,0 +1,70 @@
+# interpolate-webpack-plugin
+
+> [Extend react-dev-utils/InterpolateHtmlPlugin](https://github.com/facebookincubator/create-react-app/blob/master/packages/react-dev-utils/InterpolateHtmlPlugin.js)
+
+It works in tandem with [HtmlWebpackPlugin](https://github.com/ampedandwired/html-webpack-plugin#events).
+
+## Usage
+
+### 1. Installation
+
+```bash
+yarn add -D interpolate-webpack-plugin
+```
+
+### 2. index.html
+
+```html
+<!-- Write any variables use %VARIABLE% -->
+
+<!-- For example -->
+<script src="%INJECT_DLL%"></script>
+```
+
+### 3. Config webpack.config.js
+
+```js
+const InterpolateWebpackPlugin = require('interpolate-webpack-plugin);
+
+plugins: [
+  new InterpolateWebpackPlugin([{
+    key: 'INJECT_DLL',
+    value: 'ABSOLUTELY',
+    type: 'STRING' // type can only be 'STRING' or 'PATH', default is 'STRING'. When type is 'PATH', the 'value' you could input a glob string,like process.cwd() + 'dll/*.js',but it could only resolve matched first file name.
+  }])
+]
+```
+
+If you input wrong options or wrong type, will got a tip:
+
+#### Options wrong
+
+![Options wrong](./images/options_wrong.png)
+
+#### Type wrong
+
+![Type wrong](./images/type_wrong.png)
+
+## License
+
+MIT License
+
+Copyright (c) 2017 TaylorPzreal
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ index.js
@@ -0,0 +1,94 @@
+/**
+ * Func: interpolate custom variables into 'index.html'
+ * ext: provide path reg could interpolate path string into 'intex.html'
+ * Example: new InterpolateWebpackPlugin({
+ *  key: 'REPLACEME',
+ *  value: 'ABSOLUTELY',
+ *  type: 'STRING'
+ * })
+ *
+ * options could be an object or array like:
+ * [{
+ *  key: 'VARIABLE_NAME',
+ *  value: 'REPLACE_TEXT',
+ *  type: 'STRING' // this value could only be 'STRING' OR 'PATH' and default 'STRING'
+ * }]
+ *
+ * when 'type' is 'PATH', the 'value' coluld be an absolute string path, like: root('dll/*.js'), but only match first file path
+ */
+
+const glob = require('glob');
+const chalk = require('chalk');
+const escapeStringRegexp = require('escape-string-regexp');
+
+/**
+ * InterpolateWebpackPlugin
+ *
+ * @class InterpolateWebpackPlugin
+ */
+class InterpolateWebpackPlugin {
+  constructor(options) {
+    this.options = options;
+  }
+
+  transformPath(pathGlob) {
+    return new Promise((resolve, reject) => {
+      glob(pathGlob, (err, files) => {
+        if (err) {
+          reject(err);
+        }
+  
+        const path = files[0].replace(new RegExp(escapeStringRegexp(`${process.cwd()}/`), 'g'), '');
+  
+        resolve(path);
+      });
+    });
+  }
+
+  validateOptions(options) {
+    if (!Array.isArray(this.options)) {
+      if (typeof this.options === 'object') {
+        return [this.options];
+      } else {
+        console.error('\n', chalk.bgRed.bold('InterpolateWebpackPlugin'), 'options wrong and will be ignored.');
+        return false;
+      }
+    }
+    return options;
+  }
+
+  apply(compiler) {
+    compiler.plugin('compilation', compilation => {
+      compilation.plugin('html-webpack-plugin-before-html-processing', (data, callback) => {
+        const options = this.validateOptions(this.options);
+        if (options) {
+          (async () => {
+            const publicPath = data.assets.publicPath;
+            const len = options.length;
+  
+            for (let i = 0; i < len; i++) {
+              const item = options[i];
+              const key = new RegExp(`%${escapeStringRegexp(item.key)}%`, 'g');
+  
+              if (!item.type || item.type === 'STRING') {
+                data.html = data.html.replace(key, item.value);
+              } else if (item.type === 'PATH') {
+                await this.transformPath(item.value).then(path => {
+                  data.html = data.html.replace(key, publicPath + path);
+                });
+              } else {
+                console.warn('\n', chalk.bgYellow.bold('InterpolateWebpackPlugin'), `type '${item.type}' wrong will be ignored.`);
+              }
+            }
+  
+            callback(null, data);
+          })();
+        } else {
+          callback(null, data);
+        }
+      });
+    });
+  }
+}
+
+module.exports = InterpolateWebpackPlugin;
